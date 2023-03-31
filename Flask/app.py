9a07d8ae8f72a11bea0bd9a0d38e964436a758d0
Airman <kokyuhyun@hotmail.com>
2023-03-31 21:54:02 +0900

    flaskwebguiver

--- Flask/app.py
+++ Flask/app.py
@@ -3,8 +3,11 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import morph
+from flaskwebgui import FlaskUI
 
 app = Flask(__name__)
+
+ui = FlaskUI(app, width=500, height=500)
 
 @app.route('/')
 def index():
@@ -15,9 +18,10 @@ def termination():
     if request.method == 'POST':
         data = request.form['data']
         S1 = morph.morph(data)
-        data = S1.termination()
+        terminate_data = S1.termination()
+        
 
-        return render_template('result.html', data=data)
+        return render_template('result.html', data=terminate_data, data2 = data)
     else:
         return render_template('form.html')
     
@@ -26,4 +30,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == '__main__':
-    app.run()
+    FlaskUI(app=app, server="flask").run()
+    #app.run()
