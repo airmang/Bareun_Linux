7b13b4217c164653b3d5562f60ccbf2a599d845c
Airman_Desktop <kkokyuhyun@gmail.com>
2023-04-02 00:57:52 +0900

    v1.1

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import google.protobuf.text_format as tf
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -15,32 +16,40 @@ def index():
     return render_template("index.html")
 
 
-#문장 분석 페이지
+#문장 분석 페이지 (체언, 용언은 tokenize / 수식언, 관계언, 독립언은 morph)
 @app.route('/tokenize', methods=['GET', 'POST'])
 def tokenize():
     if request.method == 'POST':
         data = request.form['data']
         T1 = stemmer.tokenize(data)
-        tokenize_data = T1.list()
+        M1 = stemmer.morph(data)
+        seg_data = M1.list()
+        morph_data = T1.list()
+        predicates_data = M1.predicates() # 용언
+        substantives_data = M1.substantives() # 체언
+        modifier_data = M1.modifier() #수식언
+        relative_data = M1.relative() #관계언
+        termination_data = M1.termination() #어미
+        
         
 
-        return render_template('tokenize_result.html', data=tokenize_data, sentence = data)
+        return render_template('tokenize_result.html', termination_data = termination_data, relative_data = relative_data, modifier_data = modifier_data, predicates_data = predicates_data, substantives_data = substantives_data, morph_data = morph_data, seg_data = seg_data, sentence = data)
     else:
         return render_template('tokenize_form.html')
 
 
 #어미 분석 페이지
-@app.route('/termination', methods=['GET', 'POST'])
-def termination():
+@app.route('/embrace', methods=['GET', 'POST'])
+def embrace():
     if request.method == 'POST':
         data = request.form['data']
         S1 = stemmer.morph(data)
         terminate_data = S1.termination()
         
 
-        return render_template('termination_result.html', data=terminate_data, data2 = data)
+        return render_template('embrace_result.html', data=terminate_data, data2 = data)
     else:
-        return render_template('termination_form.html')
+        return render_template('embrace_form.html')
     
 @app.route('/contact')
 def contact():
--- Flask/stemmer.py
+++ Flask/stemmer.py
@@ -8,45 +8,94 @@ API_KEY="koba-YEVHS7Q-VDSUWIY-XCIS3OQ-LWD7WHA"
 tagger = Tagger(API_KEY, 'localhost',5656)
 tokenizer = Tokenizer(API_KEY, 'localhost',5656)
 
+
 class morph:
     def __init__(self, sentence):
         self.sentence = sentence
+        self.result = tagger.tags([sentence])           
+        self.morph_list = self.result.pos()
+    pos_dict = {'NNG': '일반 명사', 'NNP': '고유 명사', 'NNB': '의존 명사', 'NP': '대명사', 'NR': '수사', 'NF': '명사 추정 범주', 'NA': '분석불능범주', 'NV': '용언 추정 범주', 
+                 'VV': '동사', 'VA': '형용사', 'VX': '보조 용언', 'VCP': '긍정 지정사', 'VCN': '부정 지정사', 
+                 'MMA': '성상 관형사', 'MMD': '지시 관형사', 'MMN': '수 관형사', 'MAG': '일반 부사', 'MAJ': '접속 부사', 
+                 'IC': '감탄사', 'JKS': '주격 조사', 'JKC': '보격 조사', 'JKG': '관형격 조사', 'JKO': '목적격 조사', 'JKB': '부사격 조사', 'JKV': '호격 조사', 'JKQ': '인용격 조사', 'JX': '보조사', 'JC': '접속 조사', 
+                 'EP': '선어말 어미', 'EF': '종결 어미', 'EC': '연결 어미', 'ETN': '명사형 전성 어미', 'ETM': '관형형 전성 어미', 
+                 'XPN': '체언 접두사', 'XSN': '명사 파생 접미사', 'XSV': '동사 파생 접미사', 'XSA': '형용사 파생 접미사', 'XR': '어근', 
+                 'SF': '마침표,물음표,느낌표', 'SP': '쉼표,가운뎃점,콜론,빗금', 'SS': '따옴표,괄호표,줄표', 'SE': '줄임표', 'SO': '붙임표(물결,숨김,빠짐)', 'SW': '기타기호 (논리수학기호,화폐기호)', 'SL': '외국어', 'SH': '한자', 'SN': '숫자'}
 
+
+    def message(cls):
+        res = tagger.tags([cls.sentence])
+        return res.msg()
+
+    #어절 단위로 나누는 메소드
     def list(cls):
         res = tagger.tags([cls.sentence])
         m = res.msg()
-        print(m.sentences)
+        #print(m.sentences)
+        seg_list = []
         for sent in m.sentences:
-            for token in sent.tokens:                
-                print(token.text.content)
-            #print(m.sentences[0].tokens[0].lemma)
+            for token in sent.tokens:    
+                seg_list.append(token.text.content)
+        return seg_list
 
+    #체언 추출
+    def substantives(cls):
+        substantives_keys = {'NNG', 'NNP', 'NNB', 'NP', 'NR'}
+        filtered = [t for t in cls.morph_list if t[1] in substantives_keys]
+        new_list = [(x[0], cls.pos_dict[x[1]]) if x[1] in cls.pos_dict else x for x in filtered]
+        print(cls.morph_list)
+        print(new_list)
+        return new_list
+
+    #용언 추출
+    def predicates(cls):
+        termination_keys = {'VV', 'VA', 'VX', 'VCP', 'VCN'}
+        filtered = [t for t in cls.morph_list if t[1] in termination_keys]
+        new_list = [(x[0], cls.pos_dict[x[1]]) if x[1] in cls.pos_dict else x for x in filtered]
+        print(cls.morph_list)
+        print(new_list)
+        return new_list
+
+    #관계언 추출
+    def relative(cls):
+        termination_keys = {'JKS', 'JKC', 'JKG', 'JKO', 'JKB', 'JKV', 'JKQ', 'JX', 'JC'}
+        filtered = [t for t in cls.morph_list if t[1] in termination_keys]
+        new_list = [(x[0], cls.pos_dict[x[1]]) if x[1] in cls.pos_dict else x for x in filtered]
+        print(cls.morph_list)
+        print(new_list)
+        return new_list
+
+    #수식언 추출
+    def modifier(cls):
+        termination_keys = {'MMA', 'MMD', 'MMN', 'MAG', 'MAJ'}
+        filtered = [t for t in cls.morph_list if t[1] in termination_keys]
+        new_list = [(x[0], cls.pos_dict[x[1]]) if x[1] in cls.pos_dict else x for x in filtered]
+        print(new_list)
+        return new_list
+
+    #어미 추출
     def termination(cls):
         termination_keys = {'EP', 'EF', 'EC', 'ETN', 'ETM'}
-        result = tagger.tags([cls.sentence])           
-        morph_list = result.pos()
-        filtered = [t for t in morph_list if t[1] in termination_keys]
-        
-        print(filtered)
-        return filtered
+        filtered = [t for t in cls.morph_list if t[1] in termination_keys]
+        new_list = [(x[0], cls.pos_dict[x[1]]) if x[1] in cls.pos_dict else x for x in filtered]
+        print(cls.morph_list)
+        print(new_list)
+        return new_list
 
 class tokenize:
     def __init__(self, sentence):
         self.sentence = sentence        
-        
+
+    #형태소 단위로 나누는 메소드    
     def list(cls):
         tokenized = tokenizer.tokenize_list([cls.sentence])
         ss = tokenized.segments()
-
-        print(tokenizer.seg(cls.sentence))
         print(ss)
-        print(tokenized.print_as_json())
         return ss
     
+    
 S1 = morph('철수는 영희가 온다는 사실을 알았다.')
-S1.list()
+S1.substantives()
 P1 = tokenize('철수는 영희가 온다는 사실을 알았다.')
-#P1.list()
-
 
 
