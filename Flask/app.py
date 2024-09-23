a6f1b0e5b12c791004eca6c6a949f8a39a502cf9
KyuhyunSP11 <kokyuhyun@hotmail.com>
2024-09-23 13:59:28 +0900

    dd

--- Flask/app.py
+++ Flask/app.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import stemmer
 from flaskwebgui import FlaskUI
 
+
 app = Flask(__name__)
 
 ui = FlaskUI(app, width=500, height=500)
